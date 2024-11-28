0df815eb9b8ca974aeaf14db0a218b0c59575a95
RSYR1906 <rsim9977@gmail.com>
2024-11-28 09:49:08 +0800

    Updates

---
+++ src/main/java/com/nus/iss/edu/sg/SSF_lesson_16_example/SsfLesson16ExampleApplication.java
@@ -0,0 +1,13 @@
+package com.nus.iss.edu.sg.SSF_lesson_16_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsfLesson16ExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsfLesson16ExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nus/iss/edu/sg/SSF_lesson_16_example/SsfLesson16ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nus.iss.edu.sg.SSF_lesson_16_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsfLesson16ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
